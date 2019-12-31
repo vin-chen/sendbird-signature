1446e5d38c7615e86ec93d8f4754356553b90607
Vincent <vincent.chen02@sap.com>
2019-12-31 14:27:23 +0800

    d

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
-const PORT = process.env.PORT || 5000;
 const express = require('express');
+
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 
 app.get("/", (req, res, next)=>{
-    const twiml = new VoiceResponse();
-
-    twiml.say('Hello Vincent! Vincent! Vincent! Have fun.');
-
-    res.writeHead(200, { 'Content-Type': 'text/xml' });
-    res.end(twiml.toString());
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end("cool");
 })
 
 app.listen(PORT, ()=>{
