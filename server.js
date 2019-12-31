1c9311a40346f53c9486dbef9514af50aa8e4a5b
Vincent <vincent.chen02@sap.com>
2019-12-31 14:33:45 +0800

    xx

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res, next)=>{
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end("cool");
+})
+
+app.get("/webhook", (req, res, next)=>{
+    res.json(req);
 })
 
 app.listen(PORT, ()=>{
