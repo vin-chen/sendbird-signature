b91bb6bdb7ac4186f2c6c3a7d8dcac4e0b9ff3f8
Vincent <vincent.chen02@sap.com>
2020-01-20 16:28:09 +0800

    i

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const crypto =  require('crypto');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.use(express.text({type:'application/json',verify: (req, res, buf, encoding)=>{
-    req.rawBody = buf.toString('utf8');
+app.use(express.json({type:'application/json',verify: (req, res, buf, encoding)=>{
+    req.rawBody = buf.toString(encoding || 'utf8');
 }}));
 app.use(express.urlencoded({ extended: true }))
 
@@ -25,7 +25,8 @@ app.post("/webhook", (req, res, next) => {
     // const body = JSON.stringify(req.body);
     const result = {
         headers: req.headers,
-        body: req.body
+        body: req.body,
+        rawBody: req.rawBody
     }
     const sign = req.headers['x-sendbird-signature'];
     const hash = crypto.createHmac('sha256', MASTER_TOKEN).update(JSON.stringify(req.rawBody)).digest('hex');
