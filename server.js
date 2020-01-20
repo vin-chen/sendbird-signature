d0a7883ce1559c7f9f5533f12c6cd4107566b9c8
Vincent <vincent.chen02@sap.com>
2020-01-20 16:25:13 +0800

    i

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const crypto =  require('crypto');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.use(express.json());
+app.use(express.text({type:'application/json',verify: (req, res, buf, encoding)=>{
+    req.rawBody = buf.toString('utf8');
+}}));
 app.use(express.urlencoded({ extended: true }))
 
 
@@ -16,15 +18,17 @@ app.get("/", (req, res, next) => {
 
 app.post("/webhook", (req, res, next) => {
 
-    console.log("========================================================================");
+    console.log("========================================================================================================");
+    console.log("========================================================================================================");
+    console.log("========================================================================================================");
     
-    const body = JSON.stringify(req.body);
+    // const body = JSON.stringify(req.body);
     const result = {
         headers: req.headers,
-        body: body
+        body: req.body
     }
     const sign = req.headers['x-sendbird-signature'];
-    const hash = crypto.createHmac('sha256', MASTER_TOKEN).update(body).digest('hex');
+    const hash = crypto.createHmac('sha256', MASTER_TOKEN).update(JSON.stringify(req.rawBody)).digest('hex');
     console.log(result);
     console.log("sign: " + sign);
     console.log("hash: " + hash);
--- signature.py
+++ signature.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
 import hashlib, hmac
 
-api_token = b'my token'   # Convert a string of your API token to a bytes object
-webhook_payload = 'my payload'
+api_token = b'0a9c1297db403d32b76763c496dc0f54b22680a7'   # Convert a string of your API token to a bytes object
+webhook_payload = '{"category":"group_channel:create","created_at":1578972803,"inviter":{"nickname":"admin","user_id":"150590c940b17f8b77d2b7b1cd25b165f3012501","profile_url":"","metadata":{"role":"agent"}},"app_id":"05CE0F08-9BFC-42BB-AF65-95E42C188990","channel":{"is_super":false,"is_distinct":false,"name":"Group Channel","custom_type":"","is_ephemeral":false,"data":"","channel_url":"sendbird_group_channel_190395511_159878a38bf645fddf59cbc64bfc463afbc2b976","is_discoverable":false,"is_public":false}}'
 
 signature_to_compare = hmac.new(
     key=api_token,
